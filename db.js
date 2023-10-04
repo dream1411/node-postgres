e422b31fb2a7fdc408cf63cee3033cf9c762ccf0
DESKTOP-8C06MH9\promm <prommin.i@zealtechinter.com>
2023-10-04 18:55:10 +0700

    update

--- db.js
+++ db.js
@@ -1,12 +1,8 @@
 const Pool = require('pg').Pool;
+require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: "postgres://default:kPwAu79HUoRf@ep-proud-queen-26619993-pooler.ap-southeast-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require",
-    // user:"default",
-    // host:"ep-proud-queen-26619993-pooler.ap-southeast-1.postgres.vercel-storage.com",
-    // database:"verceldb",
-    // password:"kPwAu79HUoRf",
-    // port:5432,
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 //  user:"postgres",
 //  host:"localhost",
 //  database:"students",
